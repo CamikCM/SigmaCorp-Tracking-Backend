dff24d4acd04243810b68e4dda47713c115d508d
Abaddon <abaddonkennedy@gmail.com>
2026-01-07 01:09:27 -0400

    modified model+factory for cliente

--- database/factories/ClienteFactory.php
+++ database/factories/ClienteFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Cliente;
+use App\Models\CategoriaCliente;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +11,27 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ClienteFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Cliente::class;
+
     public function definition(): array
     {
+        $codigos = [null, 'AA', 'AAA', 'A', 'B'];
+
         return [
-            //
+            'codigo' => fake()->randomElement($codigos),
+            'abreviatura' => fake()->optional()->lexify('???'),
+            'nombre' => fake()->name(),
+            'especialidad' => fake()->optional()->randomElement([
+                'Medicina General', 'Pediatría', 'Cardiología', 'Farmacia'
+            ]),
+            'descripcion' => fake()->optional()->sentence(),
+            'direccion' => fake()->optional()->address(),
+
+            'latitud' => fake()->optional()->latitude(-19.2, -17.0),
+            'longitud' => fake()->optional()->longitude(-66.0, -64.0),
+
+            'categoria_id' => CategoriaCliente::factory(),
+            'activo' => true,
         ];
     }
 }
